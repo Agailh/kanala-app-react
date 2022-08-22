4e7204185297e917d388e3ce8c2dc7263b3ddb52
Agailh <agailham86@gmail.com>
2022-08-22 14:19:16 +0700

    fix error deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 import BG from "./assets/bg.jpg";
-import About from "./components/About";
+
 import Menu from "./components/Menu";
+import About from "./components/About";
 
 function App() {
   return (
