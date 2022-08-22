7156e9b8f759f20e0ce36fc6410da2cd80b3dd53
Agailh <agailham86@gmail.com>
2022-08-22 15:08:08 +0700

    add contact and footer

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,7 @@ import PM1 from "../assets/makanan1.jpg";
 
 const Menu = () => {
   return (
-    <div className="w-full h-auto ">
+    <div id="Menu" className="w-full h-auto ">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="uppercase text-xl tracking-widest text-white underline underline-offset-8">MENu</p>
         <h2 className="py-4 text-[#e4d7b6] text-5xl font-mono">Our Speciallity</h2>
